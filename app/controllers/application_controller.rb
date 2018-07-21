72762f17221d50e474ab1d69e6d9ca1cf719ba68
Felice Forby <feliceforby@gmail.com>
2018-07-21 05:05:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
 
-      erb :account
+      redirect '/account'
     else
       redirect '/failure'
     end
