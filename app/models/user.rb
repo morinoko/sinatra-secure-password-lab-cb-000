4416abc2d5cf23cb62bc439c1a40bbcddcc16935
Felice Forby <feliceforby@gmail.com>
2018-07-21 05:22:21 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :password
 
-  def initialize(username, password)
+  def initialize(username:, password:)
+    @username = username
+    @password = password
     @balance = 0;
   end
 end
